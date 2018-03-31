2d4cd7c853c32bf3ddb6af6ad072c8c093df0329
iosky <2061469002@qq.com>
2018-03-31 11:55:44 +0800

    表单

---
+++ 表单输入绑定/index.js
@@ -0,0 +1,22 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    msg: 'hi',
+    checked: true,
+    checkedNames: [],
+    picked: '',
+    selected: '',
+    selected2: [],
+    selected3: '',
+    options: [
+      { text: 'one', value: 'a'},
+      { text: 'two', value: 'b'},
+      { text: 'three', value: 'c'}
+    ],
+    toggle: '',
+    pick: '',
+    a: 'hello',
+    selected4: '',
+    age: 12
+  }
+})
