54888f6c551487f913ae659ea6368eff89be2700
iosky <2061469002@qq.com>
2018-04-05 22:10:49 +0800

    optimization js

--- TodoList/webpack.config.js
+++ TodoList/webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const isDev = process.env.NODE_ENV === 'development';
 const HTMLWP = require('html-webpack-plugin');
 const webpack = require('webpack');
-
+const ExtractWP = require('extract-text-webpack-plugin');
+const CleanWP = require('clean-webpack-plugin');
 
 const config = {
   target: 'web',
@@ -19,19 +20,6 @@ const config = {
       {
         test: /\.jsx$/,
         loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
       },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -51,11 +39,25 @@ const config = {
         NODE_ENV: isDev ? '"development"' : '"production"'
       }
     }),
-    new HTMLWP()
+    new HTMLWP(),
+    new CleanWP(['./dist'])
   ]
 }
 
 if (isDev) {
+  config.module.rules.push({
+    test: /\.css$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true
+        }
+      }
+    ]
+  })
   config.devtool = '#cheap-module-eval-source-map',
     config.devServer = {
       port: 8000,
@@ -73,6 +75,50 @@ if (isDev) {
       new webpack.HotModuleReplacementPlugin(),
       new webpack.NoEmitOnErrorsPlugin()
     )
+} else {
+  config.entry = {
+      app: path.join(__dirname, './src/index.js'),
+      vendor: ['vue']
+    },
+    config.output.filename = '[name].[chunkhash:8].js',
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ExtractWP.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      })
+    });
+  config.plugins.push(
+    new ExtractWP('styles.[hash:8].css'),
+  );
+  config.optimization = {
+    splitChunks: {
+      cacheGroups: { // 这里开始设置缓存的 chunks
+        commons: {
+          chunks: 'initial', // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+          minSize: 0, // 最小尺寸，默认0,
+          minChunks: 2, // 最小 chunk ，默认1
+          maxInitialRequests: 5 // 最大初始化请求书，默认1
+        },
+        vendor: {
+          test: /node_modules/, // 正则规则验证，如果符合就提取 chunk
+          chunks: 'initial', // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+          name: 'vendor', // 要缓存的 分隔出来的 chunk 名称
+          priority: 10, // 缓存组优先级
+          enforce: true
+        }
+      }
+    },
+    runtimeChunk: true
+  }
 }
 
 module.exports = config;
